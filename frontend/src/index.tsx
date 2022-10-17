04f548ebea89903402ee047fe0149b7d425f887c
Simen Sagholen Førrisdal <simensfo@stud.ntnu.no>
2022-10-17 11:59:27 +0200

    recoil wrapper and suspense

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <Suspense fallback={<div>Loading...</div>}>
+        {/* TODO: remove when adding home */}
+        <App />
+      </Suspense>
+    </RecoilRoot>
   </React.StrictMode>
 );
 
