5f8ce9fe484a18e225b4a4469001ba1d66e05c0a
Simen Sagholen Førrisdal <simensfo@stud.ntnu.no>
2022-10-19 13:26:07 +0200

    add searchbar to homepage

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import Pagination from './components/Pagination';
+import SearchBar from './components/SearchBar';
 import { getAllMovies } from './recoil/selectors';
+import './styles/Home.css';
+// TODO: Logo and logo-styling to own component?
 import './styles/HomeLogo.css';
-import { Movie } from './types';
 
 const Home = () => {
   const movies = useRecoilValue(getAllMovies);
 
   return (
     <>
-      <div className="home-logo">
-        <img src="assets/Icon.svg" alt="MovieFinder logo" />
-        <h1>MovieFinder</h1>
-        <h2>Find your movie!</h2>
-      </div>
-      {movies.data.map((movie: Movie) => (
-        <div key={movie._id}>
-          <div>{movie.Series_Title}</div>
-          <img src={movie.Poster_Link} onError={(e) => (e.currentTarget.src = 'assets/imdb_logo.svg')} />
+      <header className="home-header">
+        <div className="home-logo">
+          <img src="assets/Icon.svg" alt="MovieFinder logo" />
+          <h1>MovieFinder</h1>
+          <h2>Find your movie!</h2>
         </div>
-      ))}
+        <SearchBar />
+      </header>
+      <main className="home-content">
+        {movies.data.slice(0, 4).map((movie) => (
+          <div key={movie._id} style={{ width: 'max-content' }}>
+            <div>{movie.Series_Title}</div>
+            <img src={movie.Poster_Link} onError={(e) => (e.currentTarget.src = 'assets/imdb_logo.svg')} />
+          </div>
+        ))}
+      </main>
       <Pagination totalPages={movies.pageInfo.totalPages} />
     </>
   );
