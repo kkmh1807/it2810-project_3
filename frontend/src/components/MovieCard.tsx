89d9c3cc5644ff14f702ce74958803190d5c6d85
Kine Kai Moe Holthe <kkholthe@stud.ntnu.no>
2022-10-25 11:54:54 +0200

    Add frontend for user generated data

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -3,6 +3,7 @@ import { Movie } from '../types';
 import '../styles/MovieCard.css';
 import ExpandMore from '../assets/expand_more.svg';
 import ExpandLess from '../assets/expand_less.svg';
+import Eye from './Eye';
 
 const MovieCard = (props: { movie: Movie }) => {
   const [open, setopen] = useState(false);
@@ -13,14 +14,12 @@ const MovieCard = (props: { movie: Movie }) => {
 
   return (
     <div className="card-container">
-      <div className="movie-card" key={props.movie._id}>
+      <div className="movie-card" onClick={toggleOpen} key={props.movie._id}>
         <img id="poster" src={props.movie.Poster_Link} onError={(e) => (e.currentTarget.src = 'assets/imdb_logo.svg')} />
         <div id="title">{props.movie.Series_Title}</div>
         <div id="genre">{props.movie.Genre}</div>
         <div id="rating">{props.movie.IMDB_Rating}</div>
-        <div id="drpdwn" onClick={toggleOpen}>
-          {open ? <img src={ExpandLess} alt="" /> : <img src={ExpandMore} alt="" />}
-        </div>
+        <div id="drpdwn">{open ? <img src={ExpandLess} alt="" /> : <img src={ExpandMore} alt="" />}</div>
         {open ? <div></div> : <div id="star">{props.movie.Star1}</div>}
       </div>
       {open ? (
@@ -36,6 +35,7 @@ const MovieCard = (props: { movie: Movie }) => {
             <p>{props.movie.Star3}</p>
             <p>{props.movie.Star4}</p>
           </div>
+          <Eye watched={props.movie.Watched} movie_Id={props.movie._id} />
         </div>
       ) : (
         <div></div>
