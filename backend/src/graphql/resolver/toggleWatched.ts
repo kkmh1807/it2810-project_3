768e6ba8b333e0f300dc9b5d27dccdeadc93f3cd
Kine Kai Moe Holthe <kkholthe@stud.ntnu.no>
2022-10-27 09:11:45 +0200

    Remove source

--- backend/src/graphql/resolver/toggleWatched.ts
+++ backend/src/graphql/resolver/toggleWatched.ts
@@ -2,6 +2,5 @@ import movieModel from '../../models/movie';
 
 // Set status of a movie to watched
 export async function toggleWatched(args: { id: string; watched: boolean }) {
-  // https://stackoverflow.com/questions/67361357/how-do-i-update-a-boolean-field-to-its-opposite-in-mongoose
   return await movieModel.findByIdAndUpdate(args.id, [{ $set: { Watched: args.watched } }]);
 }
